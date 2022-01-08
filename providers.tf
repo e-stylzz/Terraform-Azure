0ccff12293ce6a37070f07915d311bbd2da94318
Eric Barb <eric.barb@gmail.com>
2022-01-07 22:34:20 -0500

    Fixed Provider

--- providers.tf
+++ providers.tf
@@ -14,8 +14,8 @@ provider "azurerm" {
   // client_secret = var.client_secret
   // client_id = var.client_id
   // tenant_id = var.tenant_id
-
-  storage_account_name = "terraform12345eb"
-  container_name = "tfstate"
-
+  backend "azurerm" {
+    storage_account_name = "terraform12345eb"
+    container_name = "tfstate"
+  }
 }
