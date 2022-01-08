db7cbc42333b1ea258173d30153dfa523b85827c
Eric Barb <eric.barb@gmail.com>
2022-01-07 22:12:30 -0500

    Initial Commit

---
+++ providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.88.1"
+    }
+  }
+}
+
+
+provider "azurerm" {
+  features  {}
+  // subscription_id = var.subscription_id
+  // client_secret = var.client_secret
+  // client_id = var.client_id
+  // tenant_id = var.tenant_id
+
+  storage_account_name = "terraform12345eb"
+  container_name = "tfstate"
+
+}
